3d817f29a3b07b66b0c3ddcd9563e809b5ce9774
Leod3c11 <leod2c11@outlook.com>
2026-09-19 12:08:11 -0300

    gpu: isolate exact OC and preserve stock dreamlte DTB

--- drivers/gpu/arm/b_r16p0/platform/exynos/gpu_custom_interface.c
+++ drivers/gpu/arm/b_r16p0/platform/exynos/gpu_custom_interface.c
@@ -143,9 +143,6 @@ static ssize_t set_clock(struct device *dev, struct device_attribute *attr, cons
 	}
 
 	if (clk == 0) {
-#if defined(CONFIG_SOC_EXYNOS8895)
-		gpu_control_restore_clock_exact(pkbdev);
-#endif
 		kbase_pm_set_policy(pkbdev, prev_policy);
 		platform->tmu_status = prev_tmu_status;
 #ifdef CONFIG_MALI_DVFS
@@ -161,34 +158,94 @@ static ssize_t set_clock(struct device *dev, struct device_attribute *attr, cons
 				break;
 			}
 		}
-#if !defined(CONFIG_SOC_EXYNOS8895)
 		platform->tmu_status = false;
-#endif
 #ifdef CONFIG_MALI_DVFS
 		if (platform->dvfs_status)
 			gpu_dvfs_on_off(false);
 #endif /* CONFIG_MALI_DVFS */
-#if defined(CONFIG_SOC_EXYNOS8895)
-		ret = gpu_control_set_clock_exact(pkbdev, clk);
-		if (ret) {
-			GPU_LOG(DVFS_ERROR, DUMMY, 0u, 0u,
-				"%s: exact G3D clock %u kHz failed (%d)\n",
-				__func__, clk, ret);
+		gpu_set_target_clk_vol(clk, false);
+		cur_state = true;
+	}
+
+	return count;
+}
+
+static ssize_t set_oc_clock(struct device *dev, struct device_attribute *attr,
+			    const char *buf, size_t count)
+{
+	unsigned int clk = 0;
+	int ret, i, policy_count;
+	static bool oc_state;
+	const struct kbase_pm_policy *const *policy_list;
+	static const struct kbase_pm_policy *prev_policy;
+	static bool prev_tmu_status = true;
+#ifdef CONFIG_MALI_DVFS
+	static bool prev_dvfs_status = true;
+#endif
+	struct exynos_context *platform =
+		(struct exynos_context *)pkbdev->platform_context;
+
+	if (!platform)
+		return -ENODEV;
+
+	ret = kstrtoint(buf, 0, &clk);
+	if (ret)
+		return -EINVAL;
+
+	if (clk == 0) {
+		if (!oc_state)
+			return count;
+
+		ret = gpu_control_restore_clock_exact(pkbdev);
+		if (ret)
+			return ret;
+
+		kbase_pm_set_policy(pkbdev, prev_policy);
+		platform->tmu_status = prev_tmu_status;
+#ifdef CONFIG_MALI_DVFS
+		if (prev_dvfs_status && !platform->dvfs_status)
+			gpu_dvfs_on_off(true);
+#endif
+		oc_state = false;
+		return count;
+	}
+
+	if (!oc_state) {
+		prev_tmu_status = platform->tmu_status;
+#ifdef CONFIG_MALI_DVFS
+		prev_dvfs_status = platform->dvfs_status;
+#endif
+		prev_policy = kbase_pm_get_policy(pkbdev);
+
+		policy_count = kbase_pm_list_policies(&policy_list);
+		for (i = 0; i < policy_count; i++) {
+			if (sysfs_streq(policy_list[i]->name, "always_on")) {
+				kbase_pm_set_policy(pkbdev, policy_list[i]);
+				break;
+			}
+		}
+
+		/* Keep thermal management enabled in exact OC mode. */
+#ifdef CONFIG_MALI_DVFS
+		if (platform->dvfs_status)
+			gpu_dvfs_on_off(false);
+#endif
+	}
+
+	ret = gpu_control_set_clock_exact(pkbdev, clk);
+	if (ret) {
+		if (!oc_state) {
 #ifdef CONFIG_MALI_DVFS
 			if (prev_dvfs_status && !platform->dvfs_status)
 				gpu_dvfs_on_off(true);
 #endif
 			platform->tmu_status = prev_tmu_status;
 			kbase_pm_set_policy(pkbdev, prev_policy);
-			cur_state = false;
-			return ret;
 		}
-#else
-		gpu_set_target_clk_vol(clk, false);
-#endif
-		cur_state = true;
+		return ret;
 	}
 
+	oc_state = true;
 	return count;
 }
 
@@ -1408,6 +1465,7 @@ static ssize_t show_cl_boost_disable(struct device *dev, struct device_attribute
  */
 
 DEVICE_ATTR(clock, S_IRUGO|S_IWUSR, show_clock, set_clock);
+DEVICE_ATTR(oc_clock, S_IWUSR, NULL, set_oc_clock);
 DEVICE_ATTR(clock_exact, S_IRUGO, show_clock_exact, NULL);
 DEVICE_ATTR(vol, S_IRUGO, show_vol, NULL);
 DEVICE_ATTR(power_state, S_IRUGO, show_power_state, NULL);
@@ -1968,6 +2026,11 @@ int gpu_create_sysfs_file(struct device *dev)
 		goto out;
 	}
 
+	if (device_create_file(dev, &dev_attr_oc_clock)) {
+		GPU_LOG(DVFS_ERROR, DUMMY, 0u, 0u, "couldn't create sysfs file [oc_clock]\n");
+		goto out;
+	}
+
 	if (device_create_file(dev, &dev_attr_clock_exact)) {
 		GPU_LOG(DVFS_ERROR, DUMMY, 0u, 0u, "couldn't create sysfs file [clock_exact]\n");
 		goto out;
@@ -2155,6 +2218,7 @@ out:
 void gpu_remove_sysfs_file(struct device *dev)
 {
 	device_remove_file(dev, &dev_attr_clock);
+	device_remove_file(dev, &dev_attr_oc_clock);
 	device_remove_file(dev, &dev_attr_clock_exact);
 	device_remove_file(dev, &dev_attr_vol);
 	device_remove_file(dev, &dev_attr_power_state);
